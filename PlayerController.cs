a346cf093fea8a0e446ad113388f884e70cfe898
Pixels <88725515+Pixelsgithub@users.noreply.github.com>
2021-08-10 19:41:10 +0530

    PlayerController.cs
    Attach it to your player

---
+++ PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = .1f;
+    
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent <Rigidbody> ();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        rigid.AddForce (moveDirection * speed);
+
+        transform.position += moveDirection * speed;
+    }
+}
